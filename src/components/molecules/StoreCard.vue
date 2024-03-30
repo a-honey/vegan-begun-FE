7ca54614910399ed45474eca105a5936df4371b0
a-honey <jungahyeon0512@khu.ac.kr>
2024-03-31 03:04:34 +0900

    feat: 컴포넌트 분리

---
+++ src/components/molecules/StoreCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="store-card">
+    <div>이미지</div>
+    <div class="store-card-container">
+      <div>가게이름</div>
+      <ul class="store-card-tags">
+        <li>태그</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'StoreCard'
+}
+</script>
+
+<style scoped>
+.store-card {
+  padding: 10px 20px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/organisms/recommend/SortTab.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="sort-tab-container">
+    <li>전체</li>
+    <li>음식점</li>
+    <li>카페</li>
+  </ul>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'SortTab'
+}
+</script>
+
+<style scoped>
+.sort-tab-container {
+  padding: 10px 20px;
+  display: flex;
+  border-radius: 5px;
+  gap: 10px;
+}
+</style>
